d11759d4ae96d4e64fd2d09df1e38544e0cf6f17
Jinwook-Song <wlsdnr129@naver.com>
2021-03-17 21:08:09 +0900

    lecture 2.13~2.17

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddelware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -12,15 +13,19 @@ import routes from "./routes";
 // express 실행한 결과를 app 상수로 저장
 const app = express();
 
+// application 보안
+app.use(helmet());
 // cookie를 전달 받아서 사용할 수 있도록 만들어주는 미들웨어 사용자 인증 같은 곳에서 쿠키를 검사할 떄 사용
 app.use(cookieParser());
 // 사용자가 웹사이트로 전달하는 정보를 검사, request 정보에서 form이나 json 형태로 된 body 검사
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// application 보안
-app.use(helmet());
 // appllication에서 발생하는 모든 일 logging
 app.use(morgan("dev"));
+
+app.set("view engine", "pug");
+
+app.use(localMiddelware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,13 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Eelete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Upload" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Video Detail" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
